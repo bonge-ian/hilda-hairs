3eb7e4839f8e7baa13e5a04bb885af17cffb7823
Bonge <nkaranjah@gmail.com>
2021-09-01 10:43:05 +0300

    Added Money Resource

---
+++ app/Cart/Money.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Cart;
+
+use Money\{Currency, Money as BaseMoney};
+use NumberFormatter;
+use Money\Currencies\ISOCurrencies;
+use Money\Formatter\IntlMoneyFormatter;
+
+class Money
+{
+    protected $money;
+
+    public function __construct(mixed $value)
+    {
+        $this->money = new BaseMoney($value, new Currency('KES'));
+    }
+
+    public function amount()
+    {
+        return $this->money->getAmount();
+    }
+
+    public function formatted()
+    {
+        $formatter = new IntlMoneyFormatter(
+            new NumberFormatter('en_KE', NumberFormatter::CURRENCY),
+            new ISOCurrencies
+        );
+        return $formatter->format($this->money);
+    }
+
+}
