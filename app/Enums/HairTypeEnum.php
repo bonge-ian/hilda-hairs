bda592c11b7c0f5e4c8be6c57f39e901e671d6af
Bonge <nkaranjah@gmail.com>
2021-09-01 10:45:31 +0300

    Added HairTypeEnum

---
+++ app/Enums/HairTypeEnum.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Enum;
+
+/**
+ * @method static self synthetic()
+ * @method static self semiHuman()
+ * @method static self human()
+ */
+class HairTypeEnum extends Enum
+{
+
+}
