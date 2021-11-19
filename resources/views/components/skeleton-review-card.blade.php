be3c4bf1ebb6f9927d0cad0ed7d3cb12bb59077c
Bonge <35810849+bonge-ian@users.noreply.github.com>
2021-11-19 18:42:45 +0300

    Added skeleton component for review comment component

---
+++ resources/views/components/skeleton-review-card.blade.php
@@ -0,0 +1,19 @@
+<article class="uk-comment skeleton-commment uk-margin">
+    <header class="uk-comment-header">
+        <div class="uk-grid-medium uk-flex-middle uk-grid" uk-grid>
+            <div class="uk-width-auto">
+                <div class="uk-comment-avatar uk-border-circle skeleton-image"></div>
+            </div>
+            <div class="uk-width-expand">
+                <div class="uk-comment-title uk-margin-remove text-transparent skeleton-title-placeholder">
+                </div>
+                <div class="uk-comment-meta uk-margin-small-top skeleton-meta-placeholder">
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="uk-comment-body skeleton-body">
+        <div class="skeleton-body-placeholder uk-width-auto uk-height-1-1">
+        </div>
+    </div>
+</article>
