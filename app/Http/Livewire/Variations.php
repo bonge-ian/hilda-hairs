014b95cf38ac3f8778c3f8b141ef511fde1e49c7
Bonge <35810849+bonge-ian@users.noreply.github.com>
2021-12-06 20:16:07 +0300

    Added variations component

--- app/Http/Livewire/Variations.php
+++ app/Http/Livewire/Variations.php
@@ -3,10 +3,16 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Cart\CartActions;
+use Gloudemans\Shoppingcart\Exceptions\CartAlreadyStoredException;
 
 class Variations extends Component
 {
     public $variations;
+
+    public $productName;
+
+    public $image;
 
     public $colors;
 
@@ -16,13 +22,26 @@ class Variations extends Component
 
     public $size;
 
-    public function mount($variations)
+    public $quantity = 1;
+
+    public $variation;
+
+    public function mount($variations, $productName, $image)
     {
         $this->variations = $variations;
+
+        $this->productName = $productName;
+
+        $this->image = $image;
 
         $this->colors = $this->groupedVariations()->keys();
 
         $this->sizes = collect();
+    }
+
+    public function getProductProperty()
+    {
+        return $this->variation->product;
     }
 
     public function updatedColor($value)
@@ -40,17 +59,91 @@ class Variations extends Component
 
     public function updatedSize($value)
     {
-        if ($value == false) {
-            $this->dispatchBrowserEvent('reset-price');
-        } else {
-            if ($this->groupedVariations()->get($this->color)->get($value)->first()->inStock()) {
-                $price = $this->groupedVariations()->get($this->color)->get($value)->pluck('price')->first()->formatted();
+        if ($value) {
+            $this->variation = $this->groupedVariations()->get($this->color)->get($value)->first();
+
+            if ($this->variation->inStock()) {
+                $price = $this->variation->price->formatted();
             } else {
                 $price = "Out of stock";
             }
 
             $this->dispatchBrowserEvent('variation-picked', ['price' => $price]);
+
+        } else {
+            $this->dispatchBrowserEvent('reset-price');
         }
+    }
+
+    public function getStockProperty()
+    {
+        return $this->variation?->stockCount() ?? 0;
+    }
+
+    public function getHasVariationProperty()
+    {
+        if (!is_null($this->variation)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function clear()
+    {
+        $this->reset([
+            'size',
+            'color',
+            'quantity',
+            'variation'
+        ]);
+
+        $this->dispatchBrowserEvent('reset-variation');
+    }
+
+    public function submit()
+    {
+        $this->validate();
+
+        try {
+            CartActions::add(
+                $this->variation,
+                $this->productName,
+                $this->quantity,
+                $this->image,
+                $this->color,
+                $this->size,
+                $this->getStockProperty()
+            );
+            $this->emit('item-added'); // will refresh the bag component
+
+            $this->dispatchBrowserEvent('item-added-to-cart', [
+                'message' => "{$this->productName} has been added to cart"
+            ]);
+        } catch (CartAlreadyStoredException) {
+            $this->dispatchBrowserEvent('item-already-stored', [
+                'message' => "{$this->productName} is already stored in your cart"
+            ]);
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.variations');
+    }
+
+    protected function rules()
+    {
+        return [
+            'color' => 'required|exists:colors,name',
+            'size' => 'required|exists:sizes,name',
+            'quantity' => [
+                'required',
+                'numeric',
+                'min:1',
+                'max:' . $this->stock
+            ]
+        ];
     }
 
     private function groupedVariations()
@@ -59,10 +152,5 @@ class Variations extends Component
             'color.name',
             'size.name'
         ])->map(fn ($sizes) => $sizes->sortKeys());
-    }
-
-    public function render()
-    {
-        return view('livewire.variations');
     }
 }
--- resources/views/livewire/variations.blade.php
+++ resources/views/livewire/variations.blade.php
@@ -1,40 +1,110 @@
 <div class="uk-panel uk-margin-medium uk-width-xlarge uk-margin-auto@m">
-    <div class="uk-grid uk-grid-small uk-flex-middle uk-child-width-1-2 uk-child-width-1-1" uk-grid>
-        <div>
-            <div class="uk-panel uk-width-1-1">
-                <form action="#">
-                    <div>
-                        <label for="color" class="uk-form-label">Color</label>
-                        <div class="uk-form-controls uk-width-1-1">
-                            <select wire:model="color" name="color" id="color" class="uk-select">
-                                <option value="0">Choose a color</option>
-                                @foreach ($colors as $color)
-                                <option wire:key="{{ $color }}" value="{{ $color }}">{{ $color }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                </form>
+    {{-- form --}}
+    <form wire:submit.prevent="submit" class="uk-form-stacked uk-grid-medium uk-grid" uk-grid>
+        <div class="uk-width-1-2@m">
+            <label for="color" class="uk-form-label">Color</label>
+            <div class="uk-form-controls uk-width-1-1">
+                <select wire:model="color"
+                    name="color" id="color" class="uk-select">
+                    <option value="0">Choose a color</option>
+                    @foreach ($colors as $color)
+                    <option wire:key="{{ $color }}" value="{{ $color }}">{{ $color }}</option>
+                    @endforeach
+                </select>
             </div>
+
+            @error('color')
+                <div class="uk-width-1-1 uk-alert uk-alert-danger" uk-alert>
+                    <p>{{ $message }}</p>
+                </div>
+            @enderror
         </div>
-        <div>
-            <div class="uk-panel uk-width-1-1">
-                <form action="#" class="uk-form-stacked">
-                    <div>
-                        <label for="size" class="uk-form-label">Size</label>
-                        <div class="uk-form-controls">
-                            <select wire:loading.attr="disabled" wire:target="color"  wire:model="size" name="size" id="size" class="uk-select" >
-
-                                <option value="0">--- Pick a color first ---</option>
-
-                                @foreach ($sizes as $size)
-                                <option wire:key="{{ $size }}" value="{{ $size }}">{{ $size }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                </form>
+        <div class="uk-width-1-2@m">
+            <label for="size" class="uk-form-label">Size</label>
+            <div class="uk-form-controls">
+                <select wire:loading.attr="disabled" wire:target="color" wire:model="size" name="size" id="size"
+                    class="uk-select">
+                    <option value="0">--- Pick a color first ---</option>
+                    @foreach ($sizes as $size)
+                    <option wire:key="{{ $size }}" value="{{ $size }}">{{ $size }}</option>
+                    @endforeach
+                </select>
             </div>
+            @error('size')
+            <div class="uk-width-1-1 uk-alert uk-alert-danger" uk-alert>
+                <p>{{ $message }}</p>
+            </div>
+            @enderror
         </div>
-    </div>
+        <div class="uk-width-1-2@m">
+            <label class="uk-form-label" for="quantity">Quantity</label>
+            <div class="uk-form-controls">
+                <input type="number" wire:model="quantity" name="quantity" id="quantity"
+                    class="uk-input" value="1" step="1" min="1" max="{{ $this->stock }}"
+                    {{ ($this->stock <= 0) ? 'disabled' : ''}}>
+
+            </div>
+            @error('quantity')
+            <div class="uk-width-1-1 uk-alert uk-alert-danger" uk-alert>
+                <p>{{ $message }}</p>
+            </div>
+            @enderror
+        </div>
+
+        <div class="uk-width-1-1 uk-margin-small " >
+            <button type="button" wire:click="clear" class="uk-button uk-button-link {{ !$this->hasVariation ? 'uk-hidden' : '' }}">
+                <span class="uk-icon" uk-icon="icon: close"></span>
+                <span>Clear </span>
+            </button>
+        </div>
+
+
+        <div class="uk-width-1-2@m">
+            <button type="submit"
+                class="uk-button uk-button-primary uk-width-1-1 uk-flex uk-flex-between uk-flex-middle">
+                <span>Add to Basket</span>
+                <span class="uk-icon" uk-icon="icon: bag"></span>
+            </button>
+        </div>
+        @auth
+            <div class="uk-width-auto">
+                <livewire:wishlist :product="$product" :ratio="1.6" :wire:key="$product->id" />
+            </div>
+        @endauth
+
+    </form>
 </div>
+
+@once
+    @push('scripts')
+        <script>
+            let priceTag = document.querySelector('.price');
+            let originalPrice = priceTag.innerText;
+
+            window.addEventListener('variation-picked', event => {
+                priceTag.innerText = event.detail.price;
+                if (event.detail.price == "Out of stock") {
+                    priceTag.classList.add('uk-text-danger');
+                } else {
+                    priceTag.classList.remove('uk-text-danger');
+                }
+            });
+
+            window.addEventListener('reset-price', event => {
+                resetPrice();
+            });
+
+            window.addEventListener('reset-variation', event => {
+                resetPrice();
+            });
+
+            function resetPrice() {
+                if (priceTag.innerText !== originalPrice) {
+                    priceTag.innerText = originalPrice;
+                    priceTag.classList.remove('uk-text-danger');
+                }
+            }
+
+        </script>
+    @endpush
+@endonce
