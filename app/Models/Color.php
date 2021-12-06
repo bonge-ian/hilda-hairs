c6889a0a3fabe4824d84ee0e057f832244eee6f5
Bonge <35810849+bonge-ian@users.noreply.github.com>
2021-12-06 20:09:19 +0300

    Added HasMany relationship on the Color Model

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Color extends Model
 {
@@ -11,5 +12,8 @@ class Color extends Model
 
     protected $fillable = ['name', 'order'];
 
-
+    public function variations(): HasMany
+    {
+        return $this->hasMany(ProductVariation::class);
+    }
 }
