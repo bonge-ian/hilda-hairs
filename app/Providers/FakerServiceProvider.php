5948bc479140ec464058fd49bbeed7aa4ecfbba4
Bonge <35810849+bonge-ian@users.noreply.github.com>
2021-12-09 20:07:50 +0300

    Added Kenyan phone numbers faker generator

--- app/Providers/FakerServiceProvider.php
+++ app/Providers/FakerServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Faker\{Factory, Generator};
+use App\Faker\KenyanPhoneNumbers;
 use Illuminate\Support\ServiceProvider;
 use Spatie\Enum\Faker\FakerEnumProvider;
 
@@ -18,6 +19,13 @@ class FakerServiceProvider extends ServiceProvider
         $this->app->singleton(Generator::class, function () {
             $faker = Factory::create();
             $faker->addProvider(new FakerEnumProvider($faker));
+
+            return $faker;
+        });
+
+        $this->app->singleton(Generator::class, function () {
+            $faker = Factory::create();
+            $faker->addProvider(new KenyanPhoneNumbers($faker));
 
             return $faker;
         });
