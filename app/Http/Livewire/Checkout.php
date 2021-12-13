2838eff3069c67a0ed44488adcc25d909d346f33
Bonge <35810849+bonge-ian@users.noreply.github.com>
2021-12-13 15:07:01 +0300

    Added Checkout Resource

---
+++ app/Http/Livewire/Checkout.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\County;
+use Livewire\Component;
+use Illuminate\Support\Collection;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
+
+class Checkout extends Component
+{
+    public string $address_1 = '';
+
+    public ?string $address_2;
+
+    public Collection $cartItems;
+
+    public bool $createAccount = false;
+
+    public string $county = '';
+
+    public Collection $counties;
+
+    public string $email = '';
+
+    public string $name = '';
+
+    public ?string $orderNotes = '';
+
+    public string $postalcode = '';
+
+    public string $phone = '';
+
+    public string $town = '';
+
+    public function mount()
+    {
+        $this->cartItems = Cart::content() ?? collect();
+        $this->counties = collect();
+
+        $this->fillFromAuthenticatedUser();
+    }
+
+    public function fetchCounties() : void
+    {
+        $this->counties = County::all();
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function render()
+    {
+        return view('livewire.checkout')
+            ->layout('layouts.app', ['title' => 'Checkout']);
+    }
+
+    protected function fillFromAuthenticatedUser()
+    {
+        if (!Auth::check()) {
+            return;
+        }
+
+        $this->fill([
+            'name' => Auth::user()->name,
+            'email' => Auth::user()->email,
+            'phone' => Auth::user()->phone,
+        ]);
+    }
+
+    protected function rules() : array
+    {
+        return [
+            'name' => [
+                'required',
+                'min:3',
+                'max:255',
+                Rule::when(Auth::check(), ['required', 'exists:users,name'])
+            ],
+            'email' => [
+                'required',
+                'email',
+                'indisposable',
+                Rule::when(Auth::check(), ['required', 'email', 'exists:users,email', 'indisposable'])
+            ],
+            'phone' => [
+                'required',
+                'phone:KE',
+                Rule::when(Auth::check(), ['required', 'exists:users,phone', 'phone:KE'])
+            ],
+            'county' => 'required|exists:counties,code|string',
+            'address_1' => 'required|string|min:6|max:255',
+            'address_2' => 'required_if:address_1|string|max:255',
+            'orderNotes' => 'sometimes|required|string|min:3',
+            'town' => 'required|string',
+            'postalCode' => 'sometimes|required|string|min:5',
+            'createAccount' => 'sometimes|accepted'
+        ];
+    }
+}
